e54e68702556b52c40f616188f10a149a1208116
kimbumsoo0820 <kbs2082@naver.com>
2022-04-09 19:33:32 +0900

    VuePrac

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
+
+<div class="black-bg" v-if="modalStatus === true">
+  <div class="white-bg">
+    <h4>상세페이지</h4>
+    <p>상세페이지 내용</p>
+    <button @click="modalStatusChange">모달 닫기</button>
+  </div>
+  
+</div>
+
+
+
+
 <div class="menu">
   <a v-for="(test,i) in menus" :key="i" >{{test}}</a>
  
 </div>
   원룸샵
   <div v-for="(products,i) in products" :key="i">
-    <h4  :style="스타일" > {{products}} 원룸</h4>
+    <img src="./assets/groynes-4434535_1920.jpg" class ="room-img"/>
+    <h4 class="RoomName" @click="modalStatusChange" :style="스타일" > {{products}} 원룸</h4>
     <h3  :style="스타일"></h3>
     <p>{{ price[i] }} 만원</p>
      <button @click="increase(i)">허위매물 신고</button>  <span> 신고수 : {{신고수[i]}} </span>
   </div>
 
-  <div >
-    <h4  :style="스타일" > {{products[0]}} 원룸</h4>
-    <h3  :style="스타일"></h3>
-    <p>{{ price[0] }} 만원</p>
-    <button @click="increase">허위매물 신고</button>  <span> 신고수 : {{신고수}} </span>
-  </div>
+  
 </template>
 
 <script>
@@ -28,6 +37,7 @@ export default {
   name: 'App',
   data(){
     return{
+      modalStatus : false,
       price : [60,70,80],
       신고수 : [0,0,0],
       스타일 : 'color : blue',
@@ -40,6 +50,9 @@ export default {
   methods : {
     increase(index) {
       this.신고수[index]++;
+    },
+    modalStatusChange(){
+      this.modalStatus = !this.modalStatus
     }
   },
 
@@ -50,6 +63,24 @@ export default {
 </script>
 
 <style>
+
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background-color: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  widows: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -67,5 +98,12 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+.room-img {
+  width: 300px;
+  margin-top: 40px;
+}
+.RoomName {
+  cursor: pointer;
 }
 </style>
