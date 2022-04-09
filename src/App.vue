3eb5ea6873742c95ade1e9a3a11dc71ba3c8d862
kimbumsoo0820 <kbs2082@naver.com>
2022-04-09 17:58:13 +0900

    VuePrac

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="menu">
+  <a v-for="(test,i) in menus" :key="i" >{{test}}</a>
+ 
+</div>
+  원룸샵
+  <div v-for="(products,i) in products" :key="i">
+    <h4  :style="스타일" > {{products}} 원룸</h4>
+    <h3  :style="스타일"></h3>
+    <p>{{ price[i] }} 만원</p>
+  </div>
+  <div >
+    <h4  :style="스타일" > {{products[0]}} 원룸</h4>
+    <h3  :style="스타일"></h3>
+    <p>{{ price[0] }} 만원</p>
+    <button @click="increase">허위매물 신고</button>  <span> 신고수 : {{신고수}} </span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      price : [60,70,80],
+      신고수 : 0,
+      스타일 : 'color : blue',
+      logo : '원룸샵',
+      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      menus : ['Home', 'Shop', 'About'],
+
+    }
+  },
+  methods : {
+    increase() {
+      this.신고수++;
+    }
+  },
+
   components: {
-    HelloWorld
+   
   }
 }
 </script>
@@ -21,6 +54,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 16px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
